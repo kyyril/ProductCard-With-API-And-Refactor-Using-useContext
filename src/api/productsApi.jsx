b24f983c74dce71a15cb737b6df2e36851d61d49
KhairilRahman04 <KhairilRahman04>
2024-06-21 02:06:20 +0700

    json api

---
+++ src/api/productsApi.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const fetchProductsApi = async() => {
+    const r = axios.get("http://localhost:3005/product")
+    return r
+}
+
+export const editProductsApi = async(id,data) => {
+    const r = axios.put(`http://localhost:3005/product/${id}`,data)
+    return r
+}
+
+export const createProductsApi = async(product) => {
+    const r = axios.post("http://localhost:3005/product",product)
+    return r
+}
+
+export const deleteProductsApi = async(id) => {
+    const r = axios.delete(`http://localhost:3005/product/${id}`)
+    return r
+}
+
