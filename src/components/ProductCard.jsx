a6c74826178c7d6c6ac14bbbe796eb5c3cf1a997
KhairilRahman04 <KhairilRahman04>
2024-06-24 17:56:53 +0700

    Refactor-useContext

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,73 +1,91 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useContext } from "react";
+import { useState } from "react";
 import { TiDelete } from "react-icons/ti";
 import { CiEdit } from "react-icons/ci";
-import ProductEdit from './ProductEdit';
+import ProductEdit from "./ProductEdit";
+import ProductContext from "../context/products";
 
-const productCard = ({product, onDeleteProducts,onEditProduct}) => {
+const productCard = ({ product }) => {
+  const { onDeleteProducts, onEditProduct } = useContext(ProductContext);
   //destructing
-  const {id,nama,deskripsi,imageURL} = product;
+  const { id, nama, deskripsi, imageURL } = product;
   const [jumlahProduct, setJumlahProduct] = useState(0);
-  const [showEdit,setShowEdit] = useState(false)
+  const [showEdit, setShowEdit] = useState(false);
 
   const tambahProduct = () => {
-    setJumlahProduct(jumlahProduct + 1)
-  }
+    setJumlahProduct(jumlahProduct + 1);
+  };
   const kurangProduct = () => {
-    setJumlahProduct(jumlahProduct - 1)
-  }
+    setJumlahProduct(jumlahProduct - 1);
+  };
 
   const handleDelete = () => {
-    onDeleteProducts(id)
-  }
+    onDeleteProducts(id);
+  };
   const handleShow = () => {
-    setShowEdit(!showEdit)
-  }
-  const onSubmitEdit = (id,data) => {//onSubmit berisi data onEdit
-    setShowEdit(!showEdit)
-    onEditProduct(id,data)
-  }
+    setShowEdit(!showEdit);
+  };
+  const onSubmitEdit = (id, data) => {
+    //onSubmit berisi data onEdit
+    setShowEdit(!showEdit);
+    onEditProduct(id, data);
+  };
   const onCancelEdit = () => {
-    setShowEdit(!showEdit)//tanpa mengubah data
-  }
-  
+    setShowEdit(!showEdit); //tanpa mengubah data
+  };
 
-    return(
-      <div className='card'>
-        {showEdit ? <ProductEdit product={product} onEditProduct = {onSubmitEdit} onCancelEdit={onCancelEdit}/>:(<>
-        <div className='edit-delete'>
-          <CiEdit onClick={handleShow} className='icon-edit'/>
-          <TiDelete onClick = {handleDelete} className='icon-delete'/>
-        </div>
-        <img style= {{
-        width:"100%",
-        height:"200px",
-        borderRadius:"10px 10px 0px 0px"}}
-        src={imageURL}
-        alt='mobil'>
-        </img>
-        <div className="container">
-          <h4>{nama}</h4>
-          <p>{deskripsi}</p>
-        </div>
-        <div className={`card-keranjang ${jumlahProduct > 0 ? "jumlah-product" : "show-keranjang"}`}>
-          {
-            jumlahProduct > 0 ? (
+  return (
+    <div className="card">
+      {showEdit ? (
+        <ProductEdit
+          product={product}
+          onEditProduct={onSubmitEdit}
+          onCancelEdit={onCancelEdit}
+        />
+      ) : (
+        <>
+          <div className="edit-delete">
+            <CiEdit onClick={handleShow} className="icon-edit" />
+            <TiDelete onClick={handleDelete} className="icon-delete" />
+          </div>
+          <img
+            style={{
+              width: "100%",
+              height: "200px",
+              borderRadius: "10px 10px 0px 0px",
+            }}
+            src={imageURL}
+            alt="mobil"
+          ></img>
+          <div className="container">
+            <h4>{nama}</h4>
+            <p>{deskripsi}</p>
+          </div>
+          <div
+            className={`card-keranjang ${
+              jumlahProduct > 0 ? "jumlah-product" : "show-keranjang"
+            }`}
+          >
+            {jumlahProduct > 0 ? (
               <>
-              <button className='button' onClick={tambahProduct}>+</button>
-              <div>{jumlahProduct}</div>
-              <button className='button' onClick={kurangProduct}>-</button>
+                <button className="button" onClick={tambahProduct}>
+                  +
+                </button>
+                <div>{jumlahProduct}</div>
+                <button className="button" onClick={kurangProduct}>
+                  -
+                </button>
               </>
-            ):
-            (
-              <div className='keranjang' onClick={tambahProduct}>+Keranjang</div>
-            )
-          }
-        </div>
+            ) : (
+              <div className="keranjang" onClick={tambahProduct}>
+                +Keranjang
+              </div>
+            )}
+          </div>
         </>
-        )}
-      </div>
-    )
-  }
+      )}
+    </div>
+  );
+};
 
 export default productCard;
--- src/components/productCreate.jsx
+++ src/components/productCreate.jsx
@@ -1,52 +1,75 @@
 // eslint-disable-next-line no-unused-vars
-import React,{useState} from 'react'
+import React, { useState } from "react";
+import { useContext } from "react";
+import ProductContext from "../context/products";
 
-const productCreate = ({ProductCreate}) => {
-    const initialState = {
-        nama: '',
-        deskripsi: '',
-        imageURL: ''
-    };
+const ProductCreate = () => {
+  const { onCreateProducts } = useContext(ProductContext);
+  const initialState = {
+    nama: "",
+    deskripsi: "",
+    imageURL: "",
+  };
 
-    const [showForm, setShowForm] = useState(false)
-    const [formData, setFormData] = useState(initialState)
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState(initialState);
 
-    const handleShow = () => {
-        setShowForm(!showForm)//setShowForm menjadi nilai kebalikan dari showForm
-    }
+  const handleShow = () => {
+    setShowForm(!showForm); //setShowForm menjadi nilai kebalikan dari showForm
+  };
 
-    const {nama,deskripsi,imageURL} = formData;
-    const handleChange = (e) => {
-        setFormData({...formData, [e.target.name]: e.target.value})
-    }
+  const { nama, deskripsi, imageURL } = formData;
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        ProductCreate(formData) //kirim ke app
-        setFormData(initialState);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreateProducts(formData); //kirim ke app
+    setFormData(initialState);
+  };
 
   return (
-    <div className='product-create'>
-        <div className='toggle-add'>
-            <button className='edit-input-submit add-toggle' onClick={handleShow}>
-                {showForm ? "Close" : "Add Product"}
-            </button>
-        </div>
-        {showForm && (
+    <div className="product-create">
+      <div className="toggle-add">
+        <button className="edit-input-submit add-toggle" onClick={handleShow}>
+          {showForm ? "Close" : "Add Product"}
+        </button>
+      </div>
+      {showForm && (
         <form className="form-group" onSubmit={handleSubmit}>
-            <div className='form-add-title'>Add Product</div>
-                <div className='form'>
-                    <input type="text" className='add-input-text' name='nama' value={nama} onChange = {handleChange} placeholder='Nama'/>
-                    <input type="text" className='add-input-text' name='deskripsi' value={deskripsi} onChange = {handleChange} placeholder='Deskripsi'/>
-                    <input type="text" className='add-input-text' name='imageURL' value={imageURL} onChange = {handleChange} placeholder='Image URL'/>
-                </div>
-                <input type="submit" className='edit-input-submit add' />
+          <div className="form-add-title">Add Product</div>
+          <div className="form">
+            <input
+              type="text"
+              className="add-input-text"
+              name="nama"
+              value={nama}
+              onChange={handleChange}
+              placeholder="Nama"
+            />
+            <input
+              type="text"
+              className="add-input-text"
+              name="deskripsi"
+              value={deskripsi}
+              onChange={handleChange}
+              placeholder="Deskripsi"
+            />
+            <input
+              type="text"
+              className="add-input-text"
+              name="imageURL"
+              value={imageURL}
+              onChange={handleChange}
+              placeholder="Image URL"
+            />
+          </div>
+          <input type="submit" className="edit-input-submit add" />
         </form>
-        )}
-        </div>
+      )}
+    </div>
+  );
+};
 
-        )
-    }
-
-export default productCreate
+export default ProductCreate;
